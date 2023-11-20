6cf5f6f9789df13ba617470588403ea6758ebf08
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-11-20 13:10:01 +0100

    marketing page route

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Medal } from "lucide-react";
+import Banner from "./components/Banner";
+
+export default function LandingPage() {
+  return (
+    <section className="w-full">
+      <Banner />
+    </section>
+  );
+}
