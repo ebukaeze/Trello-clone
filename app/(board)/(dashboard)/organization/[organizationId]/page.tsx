a82a6f79504efe12b0501d85d8c6d42f512e241e
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-01-02 23:43:08 +0100

    refactor: added create board and find all board from db

--- app/(board)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(board)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,29 @@
-import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+"use server";
 
-const OrganizationIdPage = () => {
-  const { userId, orgId } = auth();
-  return <div>{orgId}</div>;
+import { db } from "@/lib/db";
+import CreateBoard from "@/actions/create-board";
+import Board from "./board";
+import { Button } from "@/components/ui/button";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board?.findMany();
+
+  if (boards.length == 0) {
+    return <div>No boards</div>;
+  }
+
+  return (
+    <>
+      <div>
+        <CreateBoard />
+        <div className="">
+          {boards.map((board) => (
+            <Board key={board.id} title={board.title} id={board.id} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OrganizationIdPage;
